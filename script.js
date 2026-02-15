8183c1d7b4a08b86fb69b5b18fee3416a47ce52d
dlalith717 <dlalith925@gmail.com>
2026-02-15 10:16:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -66,3 +66,18 @@ function predictSymptom() {
 
     inputField.value = "";
 }
+function navigateTo(sectionId) {
+    // Hide all sections
+    const sections = document.querySelectorAll(".section");
+    sections.forEach(section => {
+        section.style.display = "none";
+    });
+
+    // Show selected section
+    const activeSection = document.getElementById(sectionId);
+    if (activeSection) {
+        activeSection.style.display = "block";
+    }
+}
+
+
